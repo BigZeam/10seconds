cea0f33141168d7d9613a87f8db3296cb6c20735
BigZeam <mpsportsguy13@gmail.com>
2023-01-22 14:28:37 -0500

    player moves

--- Assets/Code/EnemyController.cs
+++ Assets/Code/EnemyController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    public float xspeed, yspeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,11 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(xspeed * Time.deltaTime, yspeed * Time.deltaTime, 0);
+    }
+    public void SetSpeed(float x, float y)
+    {
+        xspeed = x;
+        yspeed = y;
     }
 }
---
+++ Assets/Code/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
